1b7c6f9251cc12366a88a9f863a741e285d34adf
Quenary <evgeny.savin.nord@gmail.com>
2026-03-15 04:53:24 +0300

    feat(frontend): autofocus on auth forms
    + added autofocus on login form
    + added autofocus on new password form
    + changed access modifiers
    
    Closes #140

--- frontend/src/app/features/auth/auth-form/auth-form.component.ts
+++ frontend/src/app/features/auth/auth-form/auth-form.component.ts
@@ -5,10 +5,18 @@ import { ERegexp } from 'src/app/app.consts';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
 import { IftaLabelModule } from 'primeng/iftalabel';
+import { AutoFocusModule } from 'primeng/autofocus';
 
 @Component({
   selector: 'app-auth-form',
-  imports: [TranslatePipe, ReactiveFormsModule, ButtonModule, PasswordModule, IftaLabelModule],
+  imports: [
+    TranslatePipe,
+    ReactiveFormsModule,
+    ButtonModule,
+    PasswordModule,
+    IftaLabelModule,
+    AutoFocusModule,
+  ],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,14 +25,14 @@ export class AuthFormComponent {
   public readonly isLoading = input<boolean>(false);
   public readonly OnSubmit = output<string>();
 
-  public readonly form = new FormGroup({
+  protected readonly form = new FormGroup({
     password: new FormControl<string>(null, [
       Validators.required,
       Validators.pattern(ERegexp.password),
     ]),
   });
 
-  public onSubmit(): void {
+  protected onSubmit(): void {
     if (this.form.invalid) {
       return;
     }
