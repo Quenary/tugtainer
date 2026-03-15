ae452f9193e0a53fe51b375b679cfecce813865c
Quenary <evgeny.savin.nord@gmail.com>
2026-03-15 19:16:55 +0300

    fix(agent): command/run result validation error
    + added validation of res in agent command/run endpoint
    
    Closes #152

--- agent/api/command_api.py
+++ agent/api/command_api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from pydantic import TypeAdapter
 from python_on_whales.utils import run as docker_run_cmd
 from agent.auth import verify_signature
 from agent.docker_client import DOCKER
@@ -19,7 +20,17 @@ router = APIRouter(
 )
 async def run(body: RunCommandRequestBodySchema) -> tuple[str, str]:
     _command = DOCKER.config.docker_cmd + body.command
-    return await asyncall(
+    res = await asyncall(
         lambda: docker_run_cmd(_command),
         asyncall_timeout=600,
     )
+    # although typing says that res should be a tuple,
+    # it might not be (success network connect returns empty string)
+    if not res:
+        return ("", "")
+    if isinstance(res, str):
+        return (res, "")
+    try:
+        return TypeAdapter(tuple[str, str]).validate_python(res)
+    except Exception:
+        return (str(res), "")
