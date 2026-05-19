d10a02571f550b010da372be1f36c9c4f998fa7d
Quenary <evgeny.savin.nord@gmail.com>
2026-05-19 04:02:16 +0300

    test(frontend): fix tests in ci
    + fixed error mql.addListener is not a function
    + bump node version in setup-frontend-deps action
    + add --coverage to test:ci

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -5,23 +5,20 @@ import {
 } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { AuthApiService } from './features/auth/auth-api.service';
 import { provideRouter } from '@angular/router';
 import { ToastService } from './core/services/toast.service';
 import { provideTranslateService } from '@ngx-translate/core';
-import { of } from 'rxjs';
 import {
   IsUpdateAvailableResponseBody,
   IVersion,
 } from './features/public/public-interface';
-import { MessageService } from 'primeng/api';
 import { RouterTestingHarness } from '@angular/router/testing';
-import { DialogService } from 'primeng/dynamicdialog';
 import { Mocked } from 'vitest';
 import { getToastServiceMock } from '@testing/mocks/toast-service.mock';
 import { AppStore } from './app.store';
 import { DeepSignal } from '@ngrx/signals';
-import { getAuthApiServiceMock } from '@testing/mocks/auth-api.service.mock';
+import { MenuComponent } from '@shared/components/menu/menu.component';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-test-comp',
@@ -30,21 +27,22 @@ import { getAuthApiServiceMock } from '@testing/mocks/auth-api.service.mock';
 })
 class TestComponent {}
 
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  template: '',
+})
+class MenuTestComponent {}
+
 describe('App', () => {
   let fixture: ComponentFixture<App>;
   let component: App;
 
   let harness: RouterTestingHarness;
-  let authApiServiceMock: Mocked<AuthApiService>;
   let toastServiceMock: Mocked<ToastService>;
   let appStoreMock: Partial<InstanceType<typeof AppStore>>;
 
   beforeEach(async () => {
-    authApiServiceMock = getAuthApiServiceMock();
-    authApiServiceMock.isAuthorized.mockReturnValue(of(null));
-    authApiServiceMock.isDisabled.mockReturnValue(of(false));
-    authApiServiceMock.logout.mockReturnValue(of({}));
-
     appStoreMock = {
       version: signal({
         image_version: '1.2.3',
@@ -65,17 +63,17 @@ describe('App', () => {
       providers: [
         provideZonelessChangeDetection(),
         provideTranslateService(),
-        { provide: AuthApiService, useValue: authApiServiceMock },
         { provide: AppStore, useValue: appStoreMock },
         { provide: ToastService, useValue: toastServiceMock },
+        MessageService,
         provideRouter([
           {
             path: '',
             pathMatch: 'full',
-            redirectTo: '/containers',
+            redirectTo: '/hosts',
           },
           {
-            path: 'containers',
+            path: 'hosts',
             component: TestComponent,
           },
           {
@@ -83,10 +81,17 @@ describe('App', () => {
             component: TestComponent,
           },
         ]),
-        MessageService,
-        DialogService,
       ],
-    }).compileComponents();
+    })
+      .overrideComponent(App, {
+        remove: {
+          imports: [MenuComponent],
+        },
+        add: {
+          imports: [MenuTestComponent],
+        },
+      })
+      .compileComponents();
 
     harness = await RouterTestingHarness.create();
     fixture = TestBed.createComponent(App);
@@ -106,9 +111,7 @@ describe('App', () => {
 
   it('should show toolbar', async () => {
     fixture.detectChanges();
-    await harness.navigateByUrl('/containers');
+    await harness.navigateByUrl('/hosts');
     expect(component['isToolbarVisible']()).toBe(true);
   });
-
-  // TODO add more tests
 });
